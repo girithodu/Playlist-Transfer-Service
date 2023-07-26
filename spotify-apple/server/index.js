d7bed712628bb6774948ecaaed4668e946f49b43
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-25 19:43:13 -0700

    figured out spotify authentication

--- spotify-apple/server/index.js
+++ spotify-apple/server/index.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { loginRouter } from "./routes/login.js";
+
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 const PORT = process.env.VITE_SERVER_PORT || 3000;
 app.get("/", (req, res) => {
