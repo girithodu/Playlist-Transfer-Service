0ef741f60718accb44ddf13fe94589d37a8dff6b
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-26 00:29:49 -0700

    implemented rendering single playlists

--- spotify-apple/src/Playlist/PlaylistComponent.jsx
+++ spotify-apple/src/Playlist/PlaylistComponent.jsx
@@ -1,8 +1,19 @@
-import {useState, useEffect} from 'react';
-
-const Playlist = () => {
-  return (
-    null
-  )
-}
-export default Playlist;
+import { useState, useEffect } from "react";
+import IndividualPlaylistComponent from "./IndividualPlaylistComponent.jsx";
+const PlaylistComponent = ({ playlistData, onNextHandler }) => {
+  if (playlistData) {
+    return (
+      <>
+        {playlistData.items.map((item) => {
+          return <IndividualPlaylistComponent item={item} key={item.id} />;
+        })}
+        {playlistData.previous ? (
+          <button onClick={(e) => onNextHandler("previous")}>Previous</button>
+        ) : null}
+        <button onClick={(e) => onNextHandler("next")}>Next</button>
+      </>
+    );
+  }
+  return null;
+};
+export default PlaylistComponent;
